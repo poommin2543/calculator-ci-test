b0302074b9426b958bfe697f80be545f22159958
poommin2543 <poommin2543@gmail.com>
2025-04-09 13:21:34 +0700

    add html

--- js/server.js
+++ js/server.js
@@ -1,18 +1,33 @@
+// const express = require('express');
+// const app = express();
+// const PORT = process.env.PORT || 3000;
+
+// // ตัวอย่าง API บวกเลข
+// app.get('/add', (req, res) => {
+//   const a = Number(req.query.a);
+//   const b = Number(req.query.b);
+//   res.send({ result: a + b });
+// });
+
+// app.get('/', (req, res) => {
+//   res.send('Hello from calculator CI/CD demo!');
+// });
+
+// app.listen(PORT, () => {
+//   console.log(`🚀 Server is running on port ${PORT}`);
+// });
 const express = require('express');
+const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// ตัวอย่าง API บวกเลข
-app.get('/add', (req, res) => {
-  const a = Number(req.query.a);
-  const b = Number(req.query.b);
-  res.send({ result: a + b });
-});
+// เสิร์ฟ frontend
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
-  res.send('Hello from calculator CI/CD demo!');
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 app.listen(PORT, () => {
-  console.log(`🚀 Server is running on port ${PORT}`);
+  console.log(`🚀 Server running on http://localhost:${PORT}`);
 });
